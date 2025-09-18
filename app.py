948d12789d280cd73970e87e5444c3c430bb2bd1
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-09-18 12:58:06 +0300

    SETUP DATASET

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ import plotly.express as px
 
 st.set_page_config(page_title="HR Analytics Dashboard", page_icon="📊")
 st.title("📊HR Analytics Dashboard") # title
+
+#SETUP DATASET
+conn = sqlite3.connect("hr.db") # connect with database
+df = pd.read_sql_query("SELECT * FROM employees", conn) #copy database to df

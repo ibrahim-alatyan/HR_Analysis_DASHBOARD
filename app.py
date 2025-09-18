75e8f3e4a7d47caa9c169e91ad3f59e48df2425e
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-09-18 12:57:31 +0300

    make page_import library

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import plotly.express as px
+
+st.set_page_config(page_title="HR Analytics Dashboard", page_icon="📊")
+st.title("📊HR Analytics Dashboard") # title

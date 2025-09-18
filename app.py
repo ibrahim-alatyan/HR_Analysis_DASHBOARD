6a684db60e00c3ad305a7a0f219d4d57876eab46
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-09-18 13:01:25 +0300

    TAB1_DYNAMIC_DASHBOARD

--- app.py
+++ app.py
@@ -13,3 +13,67 @@ df = pd.read_sql_query("SELECT * FROM employees", conn) #copy database to df
 
 #SPLIT TAP'S
 tab1, tab2 ,tab3 = st.tabs(["DASHBOARD","EMPLOYEES MANAGMENT","CHART"])
+
+
+#####################################################
+
+
+with tab1:
+    st.title("DYNAMIC DASHBOARD")
+    #SelectBox for filter
+    selectDep = st.selectbox("Select Department",
+                             ("All","Human Resources","Research & Development","Sales"))
+    st.write(f"**You Select {selectDep} Department**")
+    #filter
+    if selectDep != "All":
+        filterDF = df[df["Department"] == selectDep]
+    else:
+        filterDF = df
+    
+    #dynamic fig - bar chart
+    dynFig1 = px.bar(
+        filterDF.groupby("Department").size().reset_index(name = "Employees"),
+        x="Department",
+        y="Employees",
+        title="EMPLOYEES IN EVERY DEPARTMENT",
+        text="Employees",
+        color="Department"
+    )
+    st.plotly_chart(dynFig1, use_container_width=True)
+
+    #dynamic fig - pie chart
+    dynFig2 = px.pie(filterDF,
+                     names="JobRole",
+                     values="MonthlyIncome",
+                     title="MONTHLY INCOME BY JOBROLE")
+    st.plotly_chart(dynFig2, use_container_width=True)
+    
+    #dynamic fig - scatter
+    dynFig3 = px.scatter(filterDF,
+                         x = "TotalWorkingYears" , y = "MonthlyIncome",
+                         color = "TotalWorkingYears",
+                         title="Income vs Total Working Years"
+    )
+    st.plotly_chart(dynFig3, use_container_width=True)
+
+    #dynamic fig - histogram with box
+    avgIncome_byAge = (
+    filterDF.groupby("Age")["MonthlyIncome"]
+            .mean()
+            .reset_index(name= "AvgMonthlyIncome")
+    )
+    dynFig4 = px.line(avgIncome_byAge,
+                      x = "Age",
+                      y = "AvgMonthlyIncome"
+    )
+    st.plotly_chart(dynFig4, use_container_width=True)
+
+    #dynamic fig - stacked histogram 
+    dynFig5 = px.histogram(filterDF,
+                            x="JobRole", color="OverTime",
+                            title="Overtime by Job Role"
+    )
+    st.plotly_chart(dynFig5, use_container_width=True)
+
+    #TABLE
+    st.dataframe(filterDF.set_index("EmployeeNumber"))
